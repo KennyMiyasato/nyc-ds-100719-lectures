7a20e808a8098cd3acc4acac1ee799fb75756037
Kenny <kenny@miyasato.me>
2019-10-28 11:10:14 -0400

    Monday morning lecture notes from sean

---
+++ Week-3/JSON_APIs/config.py
@@ -0,0 +1,4 @@
+password = 'H3cruejjcZ37qgl'
+host = 'fis-db-instance.cepohieoey2w.us-east-1.rds.amazonaws.com'
+user = 'admin'
+yelp_api_key = 'GOD31ciSA7iVJ2jjo3gY7OOvyemIy4A5GxjGK_VwB-Y8ayMTUzG5cXlmTqX-kNLY904euzPCsczdup9fj9Il_MaFGDGaqhR8p1oWuSaEAVNK3FU8I3hL1rbvgcSxXXYx'
---
+++ Week-3/SQL/config copy.py
@@ -0,0 +1,3 @@
+password = 'H3cruejjcZ37qgl'
+host = 'fis-db-instance.cepohieoey2w.us-east-1.rds.amazonaws.com'
+user = 'admin'
